cfb5674a66016180945e67cca1a732c2d6a8fcf8
mooyoung22 <warinthorn200447@gmail.com>
2024-11-04 10:11:59 +0700

    Add files via upload

---
+++ term-2/app.js
@@ -0,0 +1,21 @@
+//let number1 ='test'
+//let number2 ='num'
+//let number3 = number1 + number2
+//console.log('New Number is',number3)
+
+// let number1 =5
+// let number2 =5
+// let condition1 = number1 >= number2
+// Boolean (true,false)
+// console.log('result of condition1 is',condition1)
+
+// if - else CSSCondition 
+// if (number1 !number2){
+//     console.log('this is if')}
+
+//     else if (number1 == number2){
+//         console.log('this is else if')}
+
+//         else {
+//             console.log('this is else')}
+
