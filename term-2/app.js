d9a14925b2fbc9fd3a693194194815f6292f5e4d
mooyoung22 <warinthorn200447@gmail.com>
2024-11-07 13:26:46 +0700

    Add files via upload

--- term-2/app.js
+++ term-2/app.js
@@ -1,3 +1,4 @@
+//   1 พฤศจิ
 //let number1 ='test'
 //let number2 ='num'
 //let number3 = number1 + number2
@@ -19,3 +20,35 @@
 //         else {
 //             console.log('this is else')}
 
+//  7 พฤศจิ
+//     เรื่องแรก
+// prompt("ให้ user ใส่อะไร")
+// let score = prompt('put you score')
+// if (score >= 80){
+//     console.log('เกรดที่ได้ : A')
+// }
+// else if (score >= 70){
+//     console.log('เกรดที่ได้ : B')
+// }
+// else if (score >= 60){
+//     console.log('เกรดที่ได้ : C')
+// }
+// else if (score >= 50){
+//     console.log('เกรดที่ได้ : D')
+// }
+// else {
+//     console.log('เกรดที่ได้ : F')
+// }
+//       เรื่องสอง
+// let text = ''
+// while(text != 'exit'){
+//     text = prompt('ใส่ข้อความมาใหม่')
+// }
+
+//      เรื่องสาม
+// let score1 = 50 เก็บทีละตัว
+// let score2 = 60 เก็บทีละตัว
+// let score = [60,50] เก็บรวม
+// score = ['ll','ss']
+// score.push()
+// console.log(score[1])
